77512093785a9a300b0c2814af1ec7a27576a8bb
ricardo padilha <padilha.ricardo@hotmail.com>
2016-06-25 07:12:24 -0300

    Logica da 1

--- T3/T3 - Tabela.sql
+++ T3/T3 - Tabela.sql
@@ -25,20 +25,25 @@ INSERT INTO ex_motorista VALUES('123AB', 'Carlo');
 •Após o cálculo o sistema deve incluir a multa na tabela ex_multa (se o contribuinte foi multado)
 •Então retornar o total acumulado de multas para o motorista.
 */
-CREATE OR REPLACE FUNCTION insereMulta(CNH CHAR(05), velAp DECIMAL(5,2)) RETURNS VOID AS $$
+CREATE OR REPLACE FUNCTION insereMulta(cnhAp CHAR(05), velAp DECIMAL(5,2)) RETURNS VOID AS $$
 DECLARE
-
-	BEGIN
-	SELECT * FROM ex_motorista mo
-	WHERE mo.cnh = CNH
-
-
-	
-RAISE NOTICE 'O motorista [nome] soma [X] pontos em em multas'
+_pontos INTEGER;
+_valor DECIMAL(9,2);
+BEGIN
+	CASE 
+	WHEN ((velAp * 0.9) > 80 AND (velAp * 0.9) <= 110) THEN SET _valor = 120;
+	WHEN ((velAp * 0.9) > 110 AND (velAp * 0.9) <= 140) THEN SET _valor = 350;
+	WHEN ((velAp * 0.9) > 140) THEN SET _valor = 680;
+	--colocar os pontos tambem
+	END
+--se multado
+	INSERT INTO ex_multa VALUES (cnhAp, velAp, valAp * 0.9, _pontos, _valor);
+RAISE NOTICE 'O motorista [%]',ex_motorista.nome ' soma [%]', SUM(ex_multas.valor)' pontos em multas';--usar where ou variaveis
+END
 $$ LANGUAGE PLPGSQL;
 
 
-/*Exercicio 02:
+/*Exercício 02:
 •Escreva um outro procedimento que atualize o campo totalMultas da tabela ex_motorista
 a partir dos totais apurados para cada motorista autuado na tabela ex_multa.
 •OBS1: motorista sem multa deverão possuir valor 0.00 no campo total multa;
